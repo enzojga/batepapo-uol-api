279ca7f64ea4d0d97d89a459bbde3311da5edaf0
Enzo <enzo202p@gmail.com>
2022-09-05 17:16:58 -0300

    Versão final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
 
@@ -18,16 +18,15 @@ mongoClient.connect().then(() => {
 
 app.post("/participants", async (req, res) => {
 
+    const { name } = req.body;
     const allParticipants = await db.collection("participants").find().toArray();
     const checkParticipants = allParticipants.filter(p => p.name === name);
 
-    const useScheme = joi.object({ name: joi.string().required(),checkParticipants: joi.array().max(0) });
-    const validation = useScheme.validate(req.body, { abortEarly: true });
+    const useScheme = joi.object({ name: joi.string().required().min(1), checkParticipants: joi.array().max(0) });
+    const validation = useScheme.validate({ ...req.body, checkParticipants }, { abortEarly: true });
 
-    const { name } = req.body;
 
     if (validation.error) {
-        console.log(validation.error.details);
         return res.sendStatus(422)
     }
 
@@ -35,6 +34,7 @@ app.post("/participants", async (req, res) => {
         name,
         lastStatus: Date.now()
     });
+
     db.collection("messages").insertOne({
         from: name,
         to: 'Todos',
@@ -49,8 +49,19 @@ app.get("/participants", (req, res) => {
     db.collection("participants").find().toArray().then(p => res.send(p));
 });
 
-app.get("/messages", (req, res) => {
-    db.collection("messages").find().toArray().then(p => res.send(p));
+app.get("/messages", async (req, res) => {
+
+    const { user } = req.headers;
+    const { limit } = req.query;
+    const allMessages = await db.collection("messages").find().toArray();
+    const filterMessages = allMessages.filter(m => m.from === user || m.to === user || m.to === "Todos");
+
+    if (limit) {
+        const limitedMessages = filterMessages.slice((-limit));
+        return res.send(limitedMessages);
+    }
+
+    res.send(filterMessages);
 })
 
 app.post("/messages", async (req, res) => {
@@ -65,22 +76,88 @@ app.post("/messages", async (req, res) => {
         type: joi.string().required(),
         sendUser: joi.array().min(1).required()
     });
-    const validate = useScheme.validate({...req.body,sendUser}, { abortEarly: true });
-    
-    if(validate.error){
+
+    const validate = useScheme.validate({ ...req.body, sendUser }, { abortEarly: true });
+
+    if (validate.error) {
         return res.sendStatus(422);
     }
 
     const { to, text, type } = req.body;
 
+    if(type !== "private_message" && type !== "message"){
+        return res.sendStatus(422);
+    }
+
     db.collection('messages').insertOne({
         to,
         text,
         type,
-        from
+        from,
+        time: dayjs().format('HH:mm:ss')
     });
 
     res.sendStatus(201);
-})
+});
+
+app.post("/status", async (req, res) => {
+
+    const { user } = req.headers;
+
+    const useScheme = joi.object({ user: joi.string().required(), objUser: joi.object().required() });
+    const objUser = await db.collection("participants").findOne({ name: user });
+    const validation = useScheme.validate({ user, objUser }, { abortEarly: true });
+
+    if (validation.error) {
+        return res.sendStatus(440);
+    }
+
+    const { _id } = objUser;
+    const newUser = { ...objUser, lastStatus: Date.now() }
+    await db.collection("participants").updateOne({ _id: _id }, { $set: newUser });
+
+    res.sendStatus(200);
+
+});
+
+app.delete("/messages/:id", async (req, res) => {
+
+    const { id } = req.params;
+    const { user } = req.headers;
+
+    const idMessage = await db.collection("messages").find({ _id: new ObjectId(id) }).toArray();
+    const useScheme = joi.object({ idMessage: joi.array().min(1) });
+    const validation = useScheme.validate({ idMessage }, { abortEarly: true });
+
+    if (validation.error) {
+        return res.sendStatus(404);
+    }
+
+    if (idMessage[0].from !== user) {
+        return res.sendStatus(401);
+    }
+
+    idMessage.forEach(m => db.collection("messages").deleteOne({ _id: m._id }));
+    res.sendStatus(200);
+});
+
+async function idleUsersRemover() {
+    const allParticipants = await db.collection("participants").find().toArray();
+    if (allParticipants.length > 0) {
+        const iddleParticipants = allParticipants.filter(p => Date.now() - p.lastStatus > 10_000);
+        iddleParticipants.forEach(p => {
+            db.collection("messages").insertOne({
+                from: p.name,
+                to: 'Todos',
+                text: 'sai da sala...',
+                type: 'status',
+                time: dayjs().format('HH:mm:ss')
+            });
+            db.collection("participants").deleteOne({ _id: p._id });
+        });
+    }
+}
+
+setInterval(idleUsersRemover, 15_000);
 
 app.listen(5000, () => console.log('Ouvindo porta 5000'));
